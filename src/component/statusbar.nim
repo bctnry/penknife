1fd4c6f1fc8f29b1cc8f4ea209f2c5fd5ecca8db
Sebastian Zack Tin Lahm-Lee <bctnry@sdf.org>
2025-03-18 21:53:36 +0000

    update

---
+++ src/component/statusbar.nim
@@ -0,0 +1,62 @@
+import std/strutils
+import sdl2
+import ../model/[state, textbuffer, style, cursor]
+import ../ui/[tvfont]
+
+const STATUS_BAR_HEIGHT*: cint = 1
+
+type
+  StatusBar* = ref object
+    parentState*: State
+    dstrect*: Rect
+    offsetX*: cint
+    offsetY*: cint
+    width*: cint
+    height*: cint
+
+proc mkStatusBar*(st: State): StatusBar =
+  return StatusBar(
+    parentState: st,
+    dstrect: (x: 0, y: 0, w: 0, h: 0),
+    offsetX: 0,
+    offsetY: 0,
+    width: 0,
+    height: 0,
+  )
+
+proc relayout*(tb: StatusBar, x: cint, y: cint, w: cint, h: cint): void =
+  tb.offsetX = x
+  tb.offsetY = y
+  tb.width = w
+  tb.height = h
+
+proc render*(renderer: RendererPtr, tb: StatusBar): void =
+  let st = tb.parentState
+  let ss = st.currentEditSession
+  tb.dstrect.x = tb.offsetX*st.gridSize.w
+  tb.dstrect.y = tb.offsetY*st.gridSize.h
+  tb.dstrect.w = tb.width*st.gridSize.w
+  tb.dstrect.h = tb.height*st.gridSize.h
+  let bgColor = st.globalStyle.getColor(STATUSBAR_BACKGROUND)
+  renderer.setDrawColor(bgColor.r, bgColor.g, bgColor.b)
+  renderer.fillRect(tb.dstrect)
+  var s = "[" & (if st.mainEditSession.textBuffer.dirty: "*" else: " ") & "] "
+  s &= (if st.focusOnAux: "^" else: "v")
+  s &= " "
+  if st.mainEditSession.selectionInEffect:
+    s &= "(" & $st.mainEditSession.selection.first.x & "," &
+               $st.mainEditSession.selection.first.y & ")-(" &
+               $st.mainEditSession.selection.last.x & "," &
+               $st.mainEditSession.selection.last.y & ")"
+  else:
+    s &= $st.mainEditSession.cursor
+  s &= " " & st.keySession.keyBuffer.join(" ")
+  discard st.globalStyle.font.renderUTF8Blended(
+    s, renderer, nil,
+    tb.dstrect.x, tb.dstrect.y,
+    st.globalStyle.getColor(STATUSBAR_FOREGROUND)
+  )
+  
+proc renderWith*(tb: StatusBar, renderer: RendererPtr): void =
+  renderer.render(tb)
+  
---
+++ src/interpret_control.nim
@@ -0,0 +1,56 @@
+import std/strutils
+import std/paths
+import model/[editsession, state, keyseq, textbuffer]
+
+# penknife control language.
+#
+# [n]   -  go to line number [n]
+# [n]+  -  go to the line [n] lines after the current line.
+# [n]-  -  go to the line [n] lines before the current line.
+# [string]>   -  search [string] from the cursor, forward.
+# [string]<   -  search [string] from the cursor, backward.
+# [string1]=[string2]>   -  replace [string1] with [string2]
+# [string1]=[string2]<   -  replace [string1] with [string2]
+# /[pat1]/=[pat2]>    -  regex replace [pat1] with [pat2]
+# /[pat1]/=[pat2]<    -  regex replace [pat1] with [pat2]
+# [id]  -  any registered command
+
+
+proc isNumber(s: string): bool =
+  for c in s:
+    if not ('0' <= c and c <= '9'): return false
+  return true
+
+proc interpretControlCommand*(s: string, st: State, shouldReload: var bool, shouldQuit: var bool): void =
+  if s.isNumber:
+    st.mainEditSession.setCursor(s.parseInt, st.mainEditSession.cursor.x)
+  elif s == "Open":
+    let aux = st.auxEditSession
+    let titleStr = aux.textBuffer.getLine(0)
+    let ss = titleStr.split('|', maxsplit=1)
+    let filePath = (if ss.len >= 2: ss[1] else: ss[0]).strip()
+    let newName = $filePath.Path.extractFilename()
+    let f = open(filePath, fmRead)
+    let s = f.readAll()
+    f.close()
+    st.loadText(s, name=newName, fullPath=filePath)
+  elif s == "Save":
+    let aux = st.auxEditSession
+    let main = st.mainEditSession
+    let titleStr = aux.textBuffer.getLine(0)
+    let ss = titleStr.split('|', maxsplit=1)
+    let filePath = (if ss.len >= 2: ss[1] else: ss[0]).strip()
+    let newName = (if ss.len >= 2: ss[0] else: $filePath.Path.extractFilename())
+    let data = main.textBuffer.toString()
+    main.textBuffer.name = newName
+    let f = open(filePath, fmWrite)
+    f.write(data)
+    f.flushFile()
+    f.close()
+    main.textBuffer.dirty = false
+  elif s == "Exit":
+    shouldReload = false
+    shouldQuit = true
+  
+  discard
+  
